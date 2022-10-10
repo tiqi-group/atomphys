84b27459562e90a9a9ea9a24aeb178ad5a9b9c39
Wojciech Jakub Adamczyk <wadamczyk@phys.ethz.ch>
2022-10-10 12:42:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='atomphys',
+    name='atomphys_tiqi',
     packages=find_packages(),
     package_data = {'': ['data/*.json'],},
     version='0.1.0',
