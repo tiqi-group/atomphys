4eb2201c044744df7a5fda94062b8c78515a7f95
Carmelo Mordini <cmordini@phys.ethz.ch>
2023-07-03 09:02:56 +0200

    Add zeeman module

---
+++ atomphys/calc/zeeman.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# Created: 07/2023
+# Author: Carmelo Mordini <cmordini@phys.ethz.ch>
+"""
+Lande factors and Zeeman spliggin for fine and hyperfine states
+
+References:
+[1] Hans A. Bethe and Edwin E. Salpeter, Quantum Mechanics of One- and Two-Electron Atoms (SpringerVerlag, Berlin, 1957).
+"""
+
+g_spin = 2.00231930436256
+
+
+def g_lande_fine(S, L, J, gL=1):
+    """
+    Calculates the Landé g-factor for fine structure. [1]
+
+    Args:
+        S (float): Total electron spin.
+        L (float): Total orbital angular momentum.
+        J (float): Total angular momentum.
+        gL (float, optional): Landé g-factor for the orbital angular momentum. Default is 1.
+
+    Returns:
+        float: Landé g-factor for the total angular momentum J.
+
+    """
+    if J == 0:
+        return 0
+    gJ = gL * (J * (J + 1) - S * (S + 1) + L * (L + 1)) / (2 * J * (J + 1)) + \
+        g_spin * (J * (J + 1) + S * (S + 1) - L * (L + 1)) / (2 * J * (J + 1))
+    return gJ
+
+
+def g_lande_hyperfine(J, I, F, gJ, gI=0):  # noqa: E741
+    """
+    Calculates the Landé g-factor for hyperfine structure. [1]
+
+    Args:
+        J (float): Total angular momentum.
+        I (float): Nuclear spin.
+        F (float): Total angular momentum (including electron and nuclear spins).
+        gJ (float): Landé g-factor for the total angular momentum J.
+        gI (float, optional): Landé g-factor for the nuclear spin. Default is 0.
+
+    Returns:
+        float: Landé g-factor for the total angular momentum F.
+
+    """
+    if F == 0:
+        return 0
+    gF = gJ * (F * (F + 1) - I * (I + 1) + J * (J + 1)) / (2 * F * (F + 1))
+    if gI != 0:
+        gF += gI * (F * (F + 1) + I * (I + 1) - J * (J + 1)) / (2 * F * (F + 1))
+    return gF
+
+
+# def zeeman_splitting(g, m, B, ureg: UnitRegistry):
+#     mu = ureg.bohr_magneton
