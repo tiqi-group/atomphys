081c84342747ec27e85e71d4c1bcc419e673a13f
Wojciech Adamczyk <wojtekadamczyk3@gmail.com>
2023-07-15 11:49:15 +0200

    Fixed unit Mess with Matrix Elements

--- atomphys/calc/matrix_element.py
+++ atomphys/calc/matrix_element.py
@@ -19,53 +19,52 @@ from .util import spherical_basis_second_rank_tensor, spherical_basis_vector
 
 
 def reduced_dipole_matrix_element(
-        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry | None = None):
+        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry):
 
-    _ureg = pint.get_application_registry() if _ureg is None else _ureg
-
-    C = _ureg('3/4*c*alpha')
-    return (C*(2*J_f+1)/k**3 * A)**(1/2)
+    return (_ureg('3/(4*c*alpha)')*(2*J_f+1)/k**3 * A)**(1/2)
 
 def reduced_electric_dipole_matrix_element(
-        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry | None = None):
+        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry):
     
-    _ureg = pint.get_application_registry() if _ureg is None else _ureg
-
-    return _ureg('e') * reduced_dipole_matrix_element(A, k, 0, _ureg)
+    return _ureg('e') * reduced_dipole_matrix_element(A, k, J_f, _ureg)
 
 def reduced_quadrupole_matrix_element(
-        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry | None = None):
-
-    _ureg = pint.get_application_registry() if _ureg is None else _ureg
-
-    C = _ureg('15/c*alpha')
-    return (C*(2*J+1)/k**5 * A)**(1/2)
+        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry):
+ 
+    return (_ureg('15/(c*alpha)')*(2*J_f+1)/k**5 * A)**(1/2)
 
 def reduced_electric_quadrupole_matrix_element(
-        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry | None = None):
-    
-    _ureg = pint.get_application_registry() if _ureg is None else _ureg
+        A: pint.Quantity, k: pint.Quantity, J_f: float, _ureg: pint.UnitRegistry):
 
     return _ureg('e') * reduced_quadrupole_matrix_element(A, k, J_f, _ureg)
 
 def dipole_matrix_element(
-        A: pint.Quantity, k: pint.Quantity, J_i: float, J_f: float, mJ_i: float, mJ_f: float, _ureg: pint.UnitRegistry | None = None):
+        A: pint.Quantity, k: pint.Quantity, J_i: float, J_f: float, mJ_i: float, mJ_f: float, _ureg: pint.UnitRegistry):
     
     dipole_matrix_element = 0
-    reduced_dipole_matrix_element = reduced_electric_dipole_matrix_element(A, k, J_f, _ureg)
+    d = reduced_dipole_matrix_element(A, k, J_f, _ureg)
     for q in range(-1, 2):
         w3j_coeff = w3j(J_i, 1, J_f, -mJ_i, q, mJ_f)
-        dipole_matrix_element += w3j_coeff * spherical_basis_vector(q) * reduced_dipole_matrix_element
+        dipole_matrix_element += w3j_coeff * spherical_basis_vector(q) * d
     return dipole_matrix_element
 
 def quadrupole_matrix_element(
-        A: pint.Quantity, k: pint.Quantity, J_i: float, J_f: float, mJ_i: float, mJ_f: float, _ureg: pint.UnitRegistry | None = None):
+        A: pint.Quantity, k: pint.Quantity, J_i: float, J_f: float, mJ_i: float, mJ_f: float, _ureg: pint.UnitRegistry):
     
     quadrupole_matrix_element = 0
-    reduced_quadrupole_matrix_element = reduced_electric_quadrupole_matrix_element(A, k, J_f, _ureg)
+    reduced_quadrupole_matrix_element = reduced_quadrupole_matrix_element(A, k, J_f, _ureg)
     for q in range(-2, 3):
         w3j_coeff = w3j(J_i, 2, J_f, -mJ_i, q, mJ_f)
         quadrupole_matrix_element += w3j_coeff * spherical_basis_second_rank_tensor(q) * reduced_quadrupole_matrix_element
     return quadrupole_matrix_element
 
+def electric_dipole_matrix_element(
+        A: pint.Quantity, k: pint.Quantity, J_i: float, J_f: float, mJ_i: float, mJ_f: float, _ureg: pint.UnitRegistry):
+    
+    return (_ureg('e') * dipole_matrix_element(A, k, J_i, J_f, mJ_i, mJ_f, _ureg))
+
+def electric_quadrupole_matrix_element(
+        A: pint.Quantity, k: pint.Quantity, J_i: float, J_f: float, mJ_i: float, mJ_f: float, _ureg: pint.UnitRegistry):
+
+    return (_ureg('e') * quadrupole_matrix_element(A, k, J_i, J_f, mJ_i, mJ_f, _ureg)).to('e*a0**2')
         
--- atomphys/calc/rabi_frequency.py
+++ atomphys/calc/rabi_frequency.py
@@ -15,8 +15,31 @@
 
 from sympy.physics.wigner import wigner_6j as w6j
 from sympy.physics.wigner import wigner_3j as w3j
+import pint
+
+def dipole_matrix_element(
+        A: pint.Quantity, k: pint.Quantity, J_i: float, J_f: float, mJ_i: float, mJ_f: float, _ureg: pint.UnitRegistry | None = None):
+    
+    dipole_matrix_element = 0
+    reduced_dipole_matrix_element = reduced_electric_dipole_matrix_element(A, k, J_f, _ureg)
+    for q in range(-1, 2):
+        w3j_coeff = w3j(J_i, 1, J_f, -mJ_i, q, mJ_f)
+        dipole_matrix_element += w3j_coeff * spherical_basis_vector(q) * reduced_dipole_matrix_element
+    return dipole_matrix_element
 
 
-def dipole_bare_Rabi_Frequency(transition, I):
+def dipole_Rabi_Frequency(
+        dipole_matrix_element: pint.Quantity, electric_field: ElectricField, _ureg: pint.UnitRegistry | None = None):
+    
+    _ureg = pint.get_application_registry() if _ureg is None else _ureg
+
+    return electric_field.amplitude * dipole_matrix_element
+
+def dipole_Rabi_Frequency(
+        transition: Transition, electric_field: ElectricField, _ureg: pint.UnitRegistry | None = None):
+    
+    _ureg = pint.get_application_registry() if _ureg is None else _ureg
+
+    return electric_field.amplitude * dipole_matrix_element(
 
 
