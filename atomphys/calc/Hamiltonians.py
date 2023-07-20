53f393aea364f4d90f2dddb0079dc7c0f0b32380
Wojciech Adamczyk <wojtekadamczyk3@gmail.com>
2023-07-20 19:29:42 +0200

    RWA hamiltonian

--- atomphys/calc/Hamiltonians.py
+++ atomphys/calc/Hamiltonians.py
@@ -31,7 +31,25 @@ def H0(atom: Atom, states: list[State], _ureg: pint.UnitRegistry):
 
     return H
 
-def H_int_graph(atom: Atom, states: list[State], fields: dict[ElectricField, list[Transition]], _ureg: pint.UnitRegistry):
+def H_int(atom: Atom, states: list[State], fields: dict[ElectricField, list[Transition]], _ureg: pint.UnitRegistry):
+    """Returns the interaction hamiltonian in rotating frame of reference for a given atom and electric fields, with states decided by the user
+    The idea is that if the electric fields do not form a closed roop, one can enter rotating frame of reference (interaction picture), such 
+    that fields either rotate at 0 frequency or the frequency much higher than the rabi frequency of any transition. This leaves the hamiltonian 
+    time independent, and so is relatively cheap and computationally easy to solve it using qutip, and hence the interesting case for it.
+
+    I am aware that for some cases, like raman transitions my algorithm woulc claim that the rotating frame of reference is not possible, but it is.
+    I dont have time right now to implement it now.
+
+    Args:
+        atom (Atom): Atom object
+        states (list[State]): List of states to be included in the hamiltonian
+        fields (dict[ElectricField, list[Transition]]): Dictionary of electric fields and transitions that are driven by them
+        _ureg (pint.UnitRegistry): Unit registry
+
+    Returns:
+        qutip.Qobj: Hamiltonian in rotating frame of reference plus correction on the base hamiltonian
+    """
+
     
     states.sort(key=lambda state: state.energy)
     states_with_mJ = []
@@ -76,38 +94,65 @@ def H_int_graph(atom: Atom, states: list[State], fields: dict[ElectricField, lis
             H += complex(RWA_shifts[i]) * ket[index] * ket[index].dag()
         
     return H
-    
 
 
-
-
-
-def sqrt_Γ_ij(state_i, state_f, transition):
-    ΔL=abs(state_f.L-state_i.L)
-    prefactor = np.sqrt((2*state_f.J + 1))
-    summation = 0
-    for q in range(-ΔL, ΔL+1): summation += wigner_3j(state_i.J, ΔL, state_f.J, -state_i.m_j, q, state_f.m_j)
-    return summation * prefactor * np.sqrt(transition.Γ)
-
-
-def Linblat_operators(kets, states, lasers):
+def collapse_operators(atom: Atom, states: list[State], fields: dict[ElectricField, list[Transition]], _ureg: pint.UnitRegistry):
     #Technically I am not doing it fully right, as I dont take under account if the photons are exactly the same
     
-    L_op = []
-    for c, laser in enumerate(lasers.keys()):
-        states_i, states_f = lasers[laser]
+    """Returns the atomic hamiltonian with no B field"""
+    H = 0
+    
+    list_all_operators = []
+    states.sort(key=lambda state: state.energy)
+    states_with_mJ = []
+    dict_states_with_mJ = {}
+    acc=0
+    for state in states:
+        J = state.quantum_numbers['J']
+        for i in range(int(2*J+1)):
+            mJ = -J+i
+            states_with_mJ.append((mJ, state))
+            dict_states_with_mJ[(mJ, state)] = acc
+            acc+=1
+    
+    total_number_of_states = len(states_with_mJ)
+    truncated_atom = atom.remove_all_but_states(states, copy=True)
+    ket = [qutip.basis(total_number_of_states, i) for i in range(total_number_of_states)]
+    
+    for i in range(len(states_with_mJ)):
+        mJ_f, state_f = states_with_mJ[-(i+1)]
+        for j in range(len(states_with_mJ)):
+            if j>i:
+                mJ_i, state_i = states_with_mJ[-(j+1)]
+                tr = atom.transition_between(state_i, state_f)
+                if tr != None:
+                    index_i = dict_states_with_mJ[(mJ_i, state_i)]
+                    index_f = dict_states_with_mJ[(mJ_f, state_f)]
+                    lo = sqrt_lindblad_operator(tr, mJ_i, mJ_f, _ureg)
+                    c_ij = complex(lo.magnitude) * (ket[index_i]*ket[index_f].dag())
+                    list_all_operators.append(c_ij)
+                    print(f'Added {complex(lo.magnitude)**2} MHz c operator from {(mJ_f, state_f.term)} to {(mJ_i, state_i.term)}')
+    
+    return list_all_operators
+
+
+
+
+    # L_op = []
+    # for c, laser in enumerate(lasers.keys()):
+    #     states_i, states_f = lasers[laser]
         
-        for i in states_f:
-            for j in states_i:
-                transitions = [i for i in filter(lambda x: (x.state_f.name == states[j].name and x.state_f.configuration==states[j].configuration), states[i].transitions)]
-                if transitions != []:
-                    transition = transitions[0]
-                    if j>i:
-                        _sqrt_Γ_ij = (sqrt_Γ_ij(states[i], states[j], transition).to('MHz**0.5')).magnitude
-                        c_ij = float(abs(_sqrt_Γ_ij)) * (ket[i]*ket[j].dag())
+    #     for i in states_f:
+    #         for j in states_i:
+    #             transitions = [i for i in filter(lambda x: (x.state_f.name == states[j].name and x.state_f.configuration==states[j].configuration), states[i].transitions)]
+    #             if transitions != []:
+    #                 transition = transitions[0]
+    #                 if j>i:
+    #                     _sqrt_Γ_ij = (sqrt_Γ_ij(states[i], states[j], transition).to('MHz**0.5')).magnitude
+    #                     c_ij = float(abs(_sqrt_Γ_ij)) * (ket[i]*ket[j].dag())
                     
-                        L_op.append(c_ij)
-    return L_op
+    #                     L_op.append(c_ij)
+    # return L_op
 
 def H_interaction(kets, states, lasers, v):
     H = 0
@@ -132,7 +177,7 @@ def H_interaction(kets, states, lasers, v):
         for i in range(len(kets)):
             if i>=min_val:
                 dH = -((laser.ω-np.dot(laser.k,v)).to('MHz')).magnitude
-                H += dH*(ket[i]*ket[i].dag())
+                H += dH*(kets[i]*ket[i].dag())
             
     H_tot = H
     
